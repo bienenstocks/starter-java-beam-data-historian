162a95c9f155004905700e0d5f5e9991a92d63d6
sarahb <sarahb@il.ibm.com>
2018-11-27 14:57:10 +0200

    readme

--- .bluemix/scripts/deploy.sh
+++ .bluemix/scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "Install jq..."
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
 chmod +x ./jq
 cp jq /usr/bin
 
+echo "Install IBM Cloud CLI..."
 if ! [ -x "$(command -v bx)" ]; then
     curl -fsSL https://clis.ng.bluemix.net/install/linux | sh
 fi
@@ -13,26 +15,36 @@ bx login --apikey $PIPELINE_API_KEY -a https://api.ng.bluemix.net
 bx target --cf
 
 # get Streaming analytics credentials
+echo "Get Streaming Analytics Credentials..."
 bx resource service-key-delete "SA_${APP_NAME}" -f
 SA_KEY=$(bx resource service-key-create "SA_${APP_NAME}" Manager --instance-name "${SA_INSTANCE}")
+API_KEY=$(echo ${SA_KEY} | awk 'BEGIN{FS="apikey: "} {print $2}' | awk '{ print $1 }')
+if [ -z "$API_KEY" ]; then
+   echo "Error generating Streaming Analytics credentials: ${SA_KEY}"
+fi
 echo "generating vcap.json"
 echo "{
     \"streaming-analytics\":[{
         \"name\" : \"streaming-analytics\",
         \"credentials\" : {
-            \"apikey\": \"$(echo ${SA_KEY} | awk 'BEGIN{FS="apikey: "} {print $2}' | awk '{ print $1 }')\",
+            \"apikey\": \"${API_KEY}\",
             \"v2_rest_url\": \"$(echo ${SA_KEY} | awk 'BEGIN{FS="v2_rest_url: "} {print $2}' | awk '{ print $1 }')\"
         }
     }]" > vcap.json
 
+# get COS credentials
 if [ $COS_INSTANCE ]; then
-    # get COS credentials
+    echo "Get Cloud Object Storage Credentials..."
     bx resource service-key-delete "COS_${APP_NAME}" -f
     COS_KEY=$(bx resource service-key-create "COS_${APP_NAME}" Manager --instance-name "${COS_INSTANCE}" --p {\"HMAC\":true})
+    API_KEY=$(echo ${COS_KEY} | awk 'BEGIN{FS="apikey: "} {print $2}' | awk '{ print $1 }')
+    if [ -z "$API_KEY" ]; then
+     echo "Error generating COS credentials: ${COS_KEY}"
+    fi
     token=$(curl -X "POST" "https://iam.bluemix.net/oidc/token" \
         -H 'Accept: application/json' \
         -H 'Content-Type: application/x-www-form-urlencoded' \
-        --data-urlencode "apikey=$(echo ${COS_KEY} | awk 'BEGIN{FS="apikey: "} {print $2}' | awk '{ print $1 }')" \
+        --data-urlencode "apikey=${API_KEY}" \
         --data-urlencode "response_type=cloud_iam" \
         --data-urlencode "grant_type=urn:ibm:params:oauth:grant-type:apikey" | jq -r '.access_token')
     curl -X "PUT" "https://s3-api.us-geo.objectstorage.softlayer.net/${APP_NAME}" \
@@ -48,16 +60,19 @@ if [ $COS_INSTANCE ]; then
         }" >> vcap.json
 fi
 
+# get MH credentials
 if [ $MH_INSTANCE ] ; then
+    echo "Get Cloud Object Storage Credentials..."
+    # Get service instance guid of MH Instance to be used to get/create a service key
     bx cf curl /v2/service_instances?q=name:"${MH_INSTANCE}" --output mh_inst_out.json
     MH_GUID=$(cat mh_inst_out.json | jq -r '.resources[] | .metadata.guid')
     bx cf curl /v2/service_keys?q=name:"MH_${APP_NAME}" --output mh_key_out.json
-
+    # If service key doesnt already exist, then create it.
     if [[ "$(cat mh_key_out.json | jq -r '.total_results')" -eq 0 ]]; then
         bx cf curl /v2/service_keys -d "{\"service_instance_guid\":\"${MH_GUID}\",\"name\":\"MH_${APP_NAME}\"}"
         bx cf curl /v2/service_keys?q=name:"MH_${APP_NAME}" --output mh_key_out.json
     fi
-    cat mh_key_out.json
+
     brokers=$(cat mh_key_out.json | jq -r '.resources[] | .entity.credentials.kafka_brokers_sasl')
     brokersstrip=$(echo $brokers | tr -d '"' | tr -d '[' | tr -d ']')
     echo ",
